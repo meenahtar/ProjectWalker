8d6f65a7538679dc7fc909e42c68051201d411a9
unknown <jas1421@GDD-4.main.ad.rit.edu>
2015-03-09 22:56:34 -0400

    Implemented note on screen and more assets

---
+++ ProjectWalker/Assets/ReadNote.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReadNote : MonoBehaviour {
+
+	GameObject note;
+
+	// Use this for initialization
+	void Start () {
+		note = GameObject.Find ("Image"); //.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyUp ("o")) {
+			/*if(note.activeSelf)
+			{
+				note.SetActive(false);
+			}
+			else
+			{
+				note.SetActive(true);
+			}*/
+			note.GetComponent<Image> ().SetActive(true);
+		}
+		
+	}
+}
