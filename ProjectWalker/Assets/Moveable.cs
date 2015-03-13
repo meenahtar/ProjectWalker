b82bb7eeafc05cb3e9cf420056bc9815701635ea
unknown <nas2375@GDD-58.main.ad.rit.edu>
2015-03-13 15:26:32 -0400

    replaced room.mb with fbx repositioned assets set canvas image inactive temporarily progress on chair movement code

---
+++ ProjectWalker/Assets/Moveable.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveable : MonoBehaviour {
+
+	GameObject playerReference;
+
+	// Use this for initialization
+	void Start () {
+		GplayerReference = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("f")) {
+			
+		}
+	}
+
+	function OnGUI(){
+		if(enter){
+			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Hold 'F' to move object");
+		}
+	}
+
+	//Activate the Main function when player is near the door
+	function OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enter = true;
+		}
+	}
+	
+	//Deactivate the Main function when player is go away from door
+	function OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enter = false;
+		}
+}
