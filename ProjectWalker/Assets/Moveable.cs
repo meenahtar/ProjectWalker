ab38bcb4ad40f081f565707775bae6105f594502
unknown <nas2375@GDD-58.main.ad.rit.edu>
2015-03-13 16:15:46 -0400

    Progress on chair movement Switched from coordinate transform to physics movement Because chair does not collide with door and can be pushed through Still need to fix collision between chair rigidbody and environment

--- ProjectWalker/Assets/Moveable.cs
+++ ProjectWalker/Assets/Moveable.cs
@@ -4,35 +4,44 @@ using System.Collections;
 public class Moveable : MonoBehaviour {
 
 	GameObject playerReference;
+	bool enterRange;
+	Vector3 difference;
+	Vector3 newPosition;
 
 	// Use this for initialization
 	void Start () {
-		GplayerReference = GameObject.FindWithTag ("Player");
+		playerReference = GameObject.FindWithTag ("Player");
+		rigidbody.WakeUp ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown ("f")) {
-			
+			difference = playerReference.transform.position - transform.position;
+		}
+		if (Input.GetKey ("f")) {
+			newPosition = new Vector3(playerReference.transform.position.x - difference.x, playerReference.transform.position.y - difference.y, playerReference.transform.position.z - difference.z);
+			rigidbody.MovePosition(newPosition);
 		}
 	}
 
-	function OnGUI(){
-		if(enter){
+	void OnGUI(){
+		if(enterRange){
 			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Hold 'F' to move object");
 		}
 	}
 
 	//Activate the Main function when player is near the door
-	function OnTriggerEnter (Collider other){
+	void OnTriggerEnter (Collider other){
 		if (other.gameObject.tag == "Player") {
-			enter = true;
+			enterRange = true;
 		}
 	}
 	
 	//Deactivate the Main function when player is go away from door
-	function OnTriggerExit (Collider other){
+	void OnTriggerExit (Collider other){
 		if (other.gameObject.tag == "Player") {
-			enter = false;
+			enterRange = false;
 		}
+	}
 }
