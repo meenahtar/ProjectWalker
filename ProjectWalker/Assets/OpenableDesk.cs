b1068c64a2c48bac76d57293fb1aec8fccc4d3a8
unknown <tjb2597@GDD-55.main.ad.rit.edu>
2015-03-13 16:36:07 -0400

    Desk open / close asset

---
+++ ProjectWalker/Assets/OpenableDesk.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenableDesk : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetKeyDown(KeyCode.Z)) {
+			// toggle visibility:
+			renderer.enabled = !renderer.enabled;
+		}
+		*/
+	}
+}
