b4ed25decf54850b8cb44fb7a23e477942ac8951
unknown <jas1421@GDD-50.main.ad.rit.edu>
2015-03-15 14:56:29 -0400

    Started read note code, not finished yet but a start

--- ProjectWalker/Assets/ReadNote.cs
+++ ProjectWalker/Assets/ReadNote.cs
@@ -4,26 +4,63 @@ using System.Collections;
 public class ReadNote : MonoBehaviour {
 
 	GameObject note;
+	bool open;
+	bool enter;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		note = GameObject.Find ("Image"); //.GetComponent<Image> ();
+		open = false;
+		enter = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
-		if (Input.GetKeyUp ("o")) {
-			if(note.activeSelf)
-			{
-				note.SetActive(false);
-			}
-			else
-			{
-				note.SetActive(true);
-			}
-			//note.GetComponent<Image> ().SetActive(true);
+		if (open) 
+		{
+			//note.Destroy();
+			note.SetActive(true);
+			print ("working");
 		}
-		
+		else
+		{
+			note.SetActive(false);
+		}
+			//note.GetComponent<Image> ().SetActive(true);
+
+
+		if(Input.GetKeyDown("f") && enter)
+		{
+			open = !open;
+		}
+
+	}
+
+	void OnGUI()
+	{
+		if(enter)
+		{
+			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to read note");
+		}
+	}
+
+	//If player enters note zone
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			enter = true;
+		}
+	}
+
+	//If player exits note zone
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") 
+		{
+			enter = false;
+		}
 	}
 }
