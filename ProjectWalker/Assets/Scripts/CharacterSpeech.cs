f17c1946c6cc924311faadc7db43f0d9564d646b
unknown <jas1421@GDD-56.main.ad.rit.edu>
2015-03-30 21:19:30 -0400

    Changed first room added speech script with speechs

--- ProjectWalker/Assets/Scripts/CharacterSpeech.cs
+++ ProjectWalker/Assets/Scripts/CharacterSpeech.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class CharacterSpeech : MonoBehaviour 
 {
+	float speechTimer;
+	bool startGame;
+	bool firstTimeThrough;
+	GUIStyle fontDetails;
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		startGame = true;
+		firstTimeThrough = true;
+		fontDetails = new GUIStyle ();
+		fontDetails.normal.textColor = Color.white;
+		fontDetails.fontSize = 20;
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -18,11 +26,26 @@ public class CharacterSpeech : MonoBehaviour
 
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open drawer");
-		/*if(enterRange && opened == false && GameObject.Find("Note2").GetComponent<ReadNote2>().getKey == true){
-			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open drawer");
-		}*/
+		if (startGame)
+		{
+			if(firstTimeThrough)
+			{
+				speechTimer = Time.time;
+				firstTimeThrough = false;
+			}
 
+			if(Time.time <= speechTimer + 10)
+			{
+
+				GUI.Label (new Rect (Screen.width / 2 - 250 , Screen.height - 100, 500, 40), "Ugh, my head. Wh- what is this place? Better have a look around.", fontDetails);
+			}
+			else
+			{
+				startGame = false;
+				firstTimeThrough = true;
+			}
+
+		}
 		
 	}
 }
