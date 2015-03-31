fb7d9166c79ebc374a06f5de2a572496edb9f4bb
unknown <jas1421@GDD-56.main.ad.rit.edu>
2015-03-30 20:40:28 -0400

    idk

--- ProjectWalker/Assets/Scripts/CharacterSpeech.cs
+++ ProjectWalker/Assets/Scripts/CharacterSpeech.cs
@@ -7,12 +7,22 @@ public class CharacterSpeech : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 	
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open drawer");
+		/*if(enterRange && opened == false && GameObject.Find("Note2").GetComponent<ReadNote2>().getKey == true){
+			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to open drawer");
+		}*/
+
+		
 	}
 }
