bbd78e2617ad70fbabdfd0bc7467dd0754dfd1ae
Paleolithic <tjb2597@rit.edu>
2015-04-01 02:31:16 -0400

    Toilet flush noise, chair movement tweaking/testing

--- ProjectWalker/Assets/Scripts/Moveable.cs
+++ ProjectWalker/Assets/Scripts/Moveable.cs
@@ -7,10 +7,17 @@ public class Moveable : MonoBehaviour {
 	bool enterRange;
 	Vector3 difference;
 	Vector3 newPosition;
+	Vector3 chairStartPos;
+
+	public int GroundSpeed = 5;
+	public int Gravity = 1;
+	public bool IsGrounded = false;
 
 	// Use this for initialization
 	void Start () {
 		playerReference = GameObject.FindWithTag ("Player");
+		chairStartPos = transform.position;
+
 		GetComponent<Rigidbody>().WakeUp ();
 	}
 	
@@ -19,9 +26,14 @@ public class Moveable : MonoBehaviour {
 		if (Input.GetKeyDown ("f") && enterRange) {
 			difference = playerReference.transform.position - transform.position;
 		}
+
+		CheckForGround();
+
 		if (Input.GetKey ("f") && enterRange) {
-			newPosition = new Vector3(playerReference.transform.position.x - difference.x, playerReference.transform.position.y - difference.y, playerReference.transform.position.z - difference.z);
-			GetComponent<Rigidbody>().MovePosition(newPosition);
+				newPosition = new Vector3 (playerReference.transform.position.x - difference.x, playerReference.transform.position.y - difference.y, playerReference.transform.position.z - difference.z);
+				GetComponent<Rigidbody> ().MovePosition (newPosition);
+		}  else if (!IsGrounded) {
+			transform.Translate(-Vector2.up * Gravity * Time.deltaTime);
 		}
 	}
 
@@ -31,13 +43,27 @@ public class Moveable : MonoBehaviour {
 		}
 	}
 
+	void CheckForGround(){
+		if(transform.position.y < chairStartPos.y)
+		{
+			// Cache the position
+			Vector3 position = transform.position;
+			// Set Y component to floor level
+			position.y = chairStartPos.y;
+			// Assign new position
+			transform.position = position;
+		}
+	}
+
 	//Activate the Main function when player is near the door
 	void OnTriggerEnter (Collider other){
 		if (other.gameObject.tag == "Player") {
-			enterRange = true;
+				enterRange = true;
+		} else {
+			enterRange = false;
 		}
 	}
-	
+
 	//Deactivate the Main function when player is go away from door
 	void OnTriggerExit (Collider other){
 		if (other.gameObject.tag == "Player") {
---
+++ ProjectWalker/Assets/Scripts/ToiletFlush.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletFlush : MonoBehaviour {
+
+	bool enterRange;
+	public AudioClip flushSound;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		enterRange = false;
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enterRange && Input.GetKeyDown ("g")){
+			source.PlayOneShot(flushSound, 4f);
+		} 
+	}
+
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enterRange = true;
+		}
+	}
+	
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enterRange = false;
+		}
+	}
+}
