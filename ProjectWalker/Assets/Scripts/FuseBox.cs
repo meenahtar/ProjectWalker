b1f0a1a9492ffe63714204084dd16ea5eab250eb
unknown <tjb2597@GDD-49.main.ad.rit.edu>
2015-04-06 18:03:23 -0400

    Light turned on after fusebox and lighter existence

--- ProjectWalker/Assets/Scripts/FuseBox.cs
+++ ProjectWalker/Assets/Scripts/FuseBox.cs
@@ -5,20 +5,35 @@ public class FuseBox : MonoBehaviour {
 
 	public bool accessable;
 	public bool lightsOn;
+	public bool firstLight;
+	
+	public float lightTimer;
+	
 	bool enterRange;
 
 	public AudioClip lightSwitchNoise; //temporary sound name
 	private AudioSource source;
 
 	GameObject darknessWall;
+	GameObject lights;
+	Light[] allChildren;
 	
 	// Use this for initialization
 	void Start () {
 		accessable = false;
 		lightsOn = false;
-
+		firstLight = true;
+		
 		source = GetComponent<AudioSource> ();
 		darknessWall = GameObject.Find ("Invisible Darkness Wall");
+		lights = GameObject.Find("Room 3 Lights");
+		
+		allChildren = lights.GetComponentsInChildren<Light>();
+		for( int i = 0; i < allChildren.Length; i++){
+			Light light = allChildren[i];
+			print (i);
+			light.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
@@ -29,6 +44,37 @@ public class FuseBox : MonoBehaviour {
 			// turn on lights in room 3
 			lightsOn = true;
 			darknessWall.SetActive(false);
+			
+			
+			
+			
+		}
+		
+		if(lightsOn){
+			if(firstLight){
+				lightTimer = Time.time;
+				firstLight = false;
+			}
+			
+			for( int i = 0; i < allChildren.Length; i++){
+				Light light = allChildren[i];
+				
+				if(i % 4 == 0 && lightTimer + 1 <= Time.time ){
+					light.enabled = true;
+				} 
+				
+				if(i % 4 == 1 && lightTimer + 2 <= Time.time){
+					light.enabled = true;
+				}
+				
+				if(i % 4 == 2 && lightTimer + 3 <= Time.time){
+					light.enabled = true;
+				}
+				
+				if(i % 4 == 3 && lightTimer + 4 <= Time.time){
+					light.enabled = true;
+				}
+			}	
 		}
 	}
 
