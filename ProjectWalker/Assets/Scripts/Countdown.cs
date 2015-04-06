c6763b4a15b88f66f97346536a9ae50d92261292
unknown <nas2375@GDD-50.main.ad.rit.edu>
2015-04-06 18:04:46 -0400

    started last room countdown code

---
+++ ProjectWalker/Assets/Scripts/Countdown.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdown : MonoBehaviour {
+
+	//set active on last door explosion
+
+	float countdownStart;
+	//set countdownStart to Time.time at set active moment
+	float timer;
+
+	void Start () {
+		countdownStart = 0.0f;
+		timer = 300.0f; //five minutes
+	}
+
+	void Update () {
+		if (countdownStart != 0.0f) {
+
+		}
+	}
+
+
+}
