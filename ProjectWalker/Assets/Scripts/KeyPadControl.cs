b45f124ebb8ce37890663e86555f1109b2c3746d
unknown <nas2375@GDD-50.main.ad.rit.edu>
2015-04-06 16:06:45 -0400

    added keypad num and back plane models materials added keypadback and keypadinitialize code (in progress)

---
+++ ProjectWalker/Assets/Scripts/KeyPadControl.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPadControl : MonoBehaviour {
+	
+	GameObject[] keyPadBacks;
+	GameObject keyPadBack1;
+	GameObject keyPadBack2;
+	GameObject keyPadBack3;
+	GameObject keyPadBack4;
+	GameObject keyPadBack5;
+	GameObject keyPadBack6;
+	GameObject keyPadBack7;
+	GameObject keyPadBack8;
+	GameObject keyPadBack9;
+
+	bool keyDown;
+	int keyCounter;
+	bool keyCodeCompleted;
+	bool codeVerification;
+	string codeAnswer;
+	string codeEntered;
+
+	float errorTimeStart;
+	float errorTimer;
+	bool errorStatus;
+	
+	// Use this for initialization
+	void Start () {
+		keyPadBacks = new GameObject[9];
+		for (int i = 0; i < 10; i++) {
+			keyPadBacks[i] = GameObject.Find("KeyPadBack" + (i + 1));
+		}
+
+		keyDown = false;
+		keyCounter = 0;
+		keyCodeCompleted = false;
+		codeVerification = false;
+		codeAnswer = "1943";
+		codeEntered = "";
+		errorTimer = 3;
+		errorStatus = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (keyCounter == 4) {
+			keyCodeCompleted = true;
+		}
+		if (Time.time - errorTimeStart > errorTimer) {
+			errorStatus = false;
+			//change all keybacks to white
+			// ---
+		}
+
+		if (!errorStatus) {
+
+			if (keyCodeCompleted) {
+
+				if (codeEntered.Equals(codeAnswer))
+				{
+					codeVerification = true;
+					//change all keybacks to correct state (green?)
+					// ---
+					//other victory reactions (final cutscene?)
+					// ---
+				}
+				else
+				{
+					//start error sequence
+					errorStatus = true;
+					errorTimeStart = Time.time;
+					//reset keycode variables
+					codeEntered = "";
+					keyCounter = 0;
+					keyCodeCompleted = false;
+					//change all keybacks to red
+					// ---
+
+				}
+
+			} else {
+
+				if (keyDown) {
+					if (Input.GetKeyUp ("1")) {
+						if (codeEntered[codeEntered.Length - 1].Equals("1"))
+						{
+							keyDown = false;
+							keyCounter = keyCounter + 1;
+							//change keyBack color to white
+							// ---
+						}
+					} else if (Input.GetKeyUp ("2")) {
+						//
+					}
+				}
+				else {
+					if (Input.GetKeyDown ("1")) {
+						keyDown = true;
+						codeEntered = codeEntered + "1";
+						//change keyBack color to pressed (grey?)
+						// ---
+						//send command to character speech
+						// ---
+					} else if (Input.GetKeyDown ("2")) {
+						//
+					} 
+				}
+				
+			}
+		}
+	}
+}
---
+++ ProjectWalker/Assets/Scripts/KeyPadInitialize.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPadInitialize : MonoBehaviour {
+
+	bool enterRange;
+	bool accessing;
+	GameObject keyPadBacks;
+
+	//first person controller
+	GameObject FPC;
+	//main camera
+	GameObject MC;
+
+	// Use this for initialization
+	void Start () {
+		keyPadBacks = GameObject.Find ("KeyPadBacks").GetComponent<keyPadControl> ();
+		keyPadBacks.SetActive (false);
+
+		FPC = GameObject.Find ("First Person Controller");
+		MC = GameObject.Find ("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enterRange && Input.GetKeyDown ("f")) {
+			//disable FPC
+			FPC.GetComponent<MouseLook>().enabled = false;
+			FPC.GetComponent<CharacterMotor>().enabled = false;
+			MC.GetComponent<MouseLook>().enabled = false;
+
+			keyPadControl.SetActive(true);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(enterRange && !accessing) {
+				GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press 'F' to use keypad");
+			}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			enterRange = true;
+		}
+	}
+	
+	//If player exits note zone
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") 
+		{
+			enterRange = false;
+		}
+	}
+}
