7f1efad130ff90656adb3c7350fe2ed1432153ba
unknown <nas2375@GDD-50.main.ad.rit.edu>
2015-04-05 22:02:59 -0400

    fusebox and darkness wall start

---
+++ ProjectWalker/Assets/Scripts/DarknessWall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarknessWall : MonoBehaviour {
+
+	GameObject FuseBox;
+	bool enterRange;
+
+	// Use this for initialization
+	void Start () {
+		FuseBox = GameObject.Find ("FuseBox");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enterRange && FuseBox.GetComponent<FuseBox>().accessable == false) 
+		{
+			FuseBox.GetComponent<FuseBox>().accessable = true;
+		}
+	}
+
+	void OnGUI(){
+		if(enterRange){
+			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "find fusebox");
+			//set variable for characterspeech?
+		}
+	}
+	
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enterRange = true;
+		}
+	}
+	
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enterRange = false;
+		}
+	}
+}
